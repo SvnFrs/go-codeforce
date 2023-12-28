b4cb8e19abeaec9249e23d376a018f6de4b4ce4c
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2023-12-28 21:53:55 +0700

    15th exercise done

---
+++ Word/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	var input string
+	fmt.Scanln(&input)
+	hasUpper := 0
+	hasLower := 0
+	for _, r := range input {
+		if unicode.IsUpper(r) {
+			hasUpper++
+			break
+		} else {
+			hasLower++
+		}
+	}
+	if hasLower >= hasUpper {
+		input = strings.ToLower(input)
+	} else {
+		input = strings.ToUpper(input)
+	}
+	fmt.Println(input)
+
+}
