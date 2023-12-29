a4f558e1703ab764f11f1c854fde9511f3e131b9
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2023-12-29 20:25:44 +0700

    18th exercise done, version 1

---
+++ AntonAndDanik/go.mod
@@ -0,0 +1,3 @@
+module example.com/antonanddanik
+
+go 1.21.5
---
+++ AntonAndDanik/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var value, Anton, Danik int
+	var input string
+	fmt.Scanln(&value)
+	fmt.Scanln(&input)
+
+	for _, char := range input {
+		if char == 'D' {
+			Danik++
+		} else {
+			Anton++
+		}
+	}
+	if Danik > Anton {
+		fmt.Println("Danik")
+	} else if Danik == Anton {
+		fmt.Println("Friendship")
+	} else {
+		fmt.Println("Anton")
+	}
+}
